6ba42822eeb52a7df5683312502fd1ca2502542b
johnbodhi <JMGarofalo@gmail.com>
2022-11-22 18:38:27 -0600

    Signal Play

--- signalPlay.m
+++ signalPlay.m
@@ -1,6 +1,6 @@
 clear all; close all; clc;
 
-f0 = 10; % f0 needs to be greater than or equal to N...
+f0 = 1.001; % The fundamental frequency needs to be greater than 1...
 
 D = 2; % D needs to be greater than or equal to 2 in the integers... 
 
@@ -17,7 +17,8 @@ m = ( 0:1:size( t, 2 ) - 1 );
 F = 1; uu = 1;
 for i = 1:1:size(t, 2)
 
-    % Find frequency infemum...
+    % Find hamonic frequency infemum. This ensures no aliasing in the
+    % system.
 
     if( m( i ) * f0 >= Fs / D )
 
@@ -32,26 +33,54 @@ if( G )
     % We need to clip the time inteval to include only those frequencies
     % satified by the Shannon-Nyquist Theorem...
 
-    T = t( 1:1:2*F( 1, 1 ) ); M = m( 1:1:size( T, 2 ) );
+    T = t( 1:1:2 * F( 1, 1 ) ); M = m( 1:1:size( T, 2 ) );
 
     % Power waveform...
 
-    X = cos( 2 * pi * f0 * M .* T ) + sin( ( 2 * pi * f0 * ( M + 1 ) .* T ) - phi ); % This generates a superposition of power harmomics.
+    Y = cos( 2 * pi * f0 * M .* T ) + sin( ( 2 * pi * f0 * ( M + 1 ) .* T ) - phi ); % This generates a superposition of power harmomics.
 else 
 
-    x = cos( 2 * pi * f0 * m .* t ) + sin( 2 * pi * f0 * ( m + 1 ) .* t - phi );
+    y = cos( 2 * pi * f0 * m .* t ) + sin( 2 * pi * f0 * ( m + 1 ) .* t - phi );
 end
 
 H = Fs / f0;
 
-plot( T(1:1:H), X(1:1:H), 'k' ); hold on;
+% plot( T(1:1:H), X(1:1:H), 'k' ); hold on;
 
-pause(0.5);
+% pause(0.5);
+% 
+% Y = 10; SECONDS = 10 * Y; 
+% 
+% for i = 1:1:SECONDS
+% 
+%     sound( X, Fs );
+% end
 
-Y = 10; SECONDS = 10 * Y; 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-for i = 1:1:SECONDS
+[ z, Fs ] = audioread('John Legend - Nervous _Lyrics_.mp4');
 
-    sound( X, Fs );
-end
+LB = size( z, 1 ) / 2 - size( T, 2 ) / 2; 
+
+UB = size( z, 1 ) / 2 + size( T, 2 ) / 2 - 1;
+
+C(1,:) = z( LB:UB, 1);
+
+% sound( C, S( 1, 3 ) );
+
+SIG = sum( abs( C ), 2 ); NOI = sum( abs( Y ), 2 );
+
+s = rng;
+
+AWGN = sqrt( SIG / NOI ) .* randn( 1, size(C, 2) );
+
+X = cos( 2 * pi * f0 * M .* T ) + sin( ( 2 * pi * f0 * ( M + 1 ) .* T ) - phi ) + C(1,1:end) + AWGN;
+
+plot( T(1:1:H), X(1:1:H), 'g' ); hold on;
+plot( T(1:1:H), AWGN(1:1:H), 'm' ); hold on;
+plot( T(1:1:H), Y(1:1:H), 'k' ); hold on; 
+
+sound( X, S( 1, 3 ) );
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
